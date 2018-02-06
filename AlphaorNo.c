3cad463be6459fd38a8ca81f71abb3ff1cf058d8
nandhinic2795 <30898117+nandhinic2795@users.noreply.github.com>
2018-02-06 22:53:33 +0530

    Create AlphaorNo.c

---
+++ AlphaorNo.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+	char ch;
+	scanf("%c",&ch);
+	if(ch>='a'&&ch<='z')
+		printf("Alphabet");
+		else
+		printf("No");
+	
+	// your code goes here
+	return 0;
+}
