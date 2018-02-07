06dfd9e3601da0d0b3ba35b17bc30ef5414cb926
nandhinic2795 <30898117+nandhinic2795@users.noreply.github.com>
2018-02-07 11:14:21 +0530

    Create Kintegersadd.c

---
+++ Kintegersadd.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+	int n,i,k,sum=0,a[100];
+	scanf("%d\n",&n);
+	scanf("%d\n",&k);
+	for(i=0;i<n;i++)
+	{
+            scanf("%d",&a[i]);
+	}
+	for(i=0;i<k;i++)
+	{
+		sum+=a[i];
+	}
+printf("%d",sum);	// your code goes here
+	return 0;
+}
