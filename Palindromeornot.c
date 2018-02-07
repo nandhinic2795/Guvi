89339134f2fd137edc97a412ef85045616f616ce
nandhinic2795 <30898117+nandhinic2795@users.noreply.github.com>
2018-02-07 11:38:06 +0530

    Rename Palindrome.c to Palindromeornot.c

---
+++ Palindromeornot.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void) {
+	int n,r,a,sum=0;
+	scanf("%d",&n);
+	a=n;
+while(a!=0)
+{
+	r=a%10;
+	sum=sum*10+(r);
+	a=a/10;
+}
+if(n==sum)
+{
+	printf("Palindrome");
+}
+else
+{
+	printf("Not palindrome");
+}
+
+// your code goes here
+	return 0;
+}
+
