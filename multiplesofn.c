16c69bd0d11c64081371b01e79b46639d747644c
nandhinic2795 <30898117+nandhinic2795@users.noreply.github.com>
2018-02-07 11:53:14 +0530

    Create multiplesofn.c

---
+++ multiplesofn.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	int n1,n,i,c;
+	scanf("%d",&n1);
+	n=n1;
+	for(i=1;i<=n1;i++)
+	{
+	c=n*i;
+	printf("%d ",c);
+	}
+	
+	
+	// your code goes here
+	return 0;
+}
