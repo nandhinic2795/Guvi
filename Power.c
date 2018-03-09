6ee5b2279779c620a8ca7fe2bb7b282deb15f12c
nandhinic2795 <30898117+nandhinic2795@users.noreply.github.com>
2018-03-09 12:48:14 +0530

    Create Power.c

---
+++ Power.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+
+int main()
+{
+ int a,b,i;
+ int c;
+ scanf("%d",&a);
+ scanf("%d",&b);
+ c=1;
+ for(i=1;i<=b;i++)
+ {
+     c=c*a;
+ }
+ printf("%d",c);
+
+    return 0;
+}
+
+
+
