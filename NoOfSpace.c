cb8f97bf5766015b5e31fb4d5ef95c118e2f6392
nandhinic2795 <30898117+nandhinic2795@users.noreply.github.com>
2018-03-13 00:29:33 +0530

    Create NoOfSpace.c

---
+++ NoOfSpace.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+int main()
+{
+    char str[50];
+    int i,count=1;
+    scanf("%[^\n]s",str);
+    for(i=0;str[i]!='\0';i++){
+        if(str[i]==' '){
+            count++;
+        }
+    }
+    printf("%d",count);
+    return 0;
+}
