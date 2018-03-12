9c83111fb4434051d9289e1dbedfb154be24477a
nandhinic2795 <30898117+nandhinic2795@users.noreply.github.com>
2018-03-13 00:17:35 +0530

    Update Countchar.c

--- Countchar.c
+++ Countchar.c
@@ -6,11 +6,12 @@ int main()
     char str[50];
     int i,count;
     scanf("%[^\n]s",str);
-    for(i=0;i<=50;i++)
-    if((str[i]>='a'&&str[i]<='z')||(str[i]>='A'&&str[i]<='Z'))
-    {
+    for(i=0;str[i]!='\0';i++){
+        if(str[i]==' '){
+        }
+        else
         count++;
     }
-printf("%d",count);
+    printf("%d",count);
     return 0;
 }
