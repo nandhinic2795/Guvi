a52d47460d09af25502964c745bf9307881a55f1
nandhinic2795 <30898117+nandhinic2795@users.noreply.github.com>
2018-03-13 14:06:22 +0530

    Create NoGivenrange.c

---
+++ NoGivenrange.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+         int n;
+         scanf("%d",&n);
+         if(n>0&&n<10)
+         {
+             printf("yes");
+         }
+         else
+         {
+             printf("No");
+         }
+	return 0;
+}
