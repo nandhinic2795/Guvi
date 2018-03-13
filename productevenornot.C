368e3f4c289076dd8414e8f0f4295068888e011b
nandhinic2795 <30898117+nandhinic2795@users.noreply.github.com>
2018-03-13 14:59:19 +0530

    Create productevenornot.C

---
+++ productevenornot.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+         int n2,n1,p;
+         scanf("%d",&n1);
+        scanf("%d",&n2);
+        p=n1*n2;
+        if(p%2==0){
+          printf("Even");  
+        }
+        
+        else{
+          printf("Odd");
+        }
+        
+         	return 0;
+}
