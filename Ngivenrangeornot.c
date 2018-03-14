2348b64c0cf554067cd7bb14c32edc8897e3b6b7
nandhinic2795 <30898117+nandhinic2795@users.noreply.github.com>
2018-03-14 18:36:53 +0530

    Create Ngivenrangeornot.c

---
+++ Ngivenrangeornot.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void) {
+int n;
+int L,R;
+scanf("%d",&n);
+scanf("%d",&L);
+scanf("%d",&R);
+	if(n>L&&n<R){
+	printf("yes");
+	return;
+	}
+printf("No");
+	// your code goes here
+	return 0;
+}
