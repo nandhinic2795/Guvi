41df120a06952e930b87c7a6bb624519a34c64f9
nandhinic2795 <30898117+nandhinic2795@users.noreply.github.com>
2018-03-15 18:52:14 +0530

    Create Swap.c

---
+++ Swap.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+         int n,n1;
+         scanf("%d",&n1);
+        scanf("%d",&n);
+        n1=(n1+n)-(n=n1);
+        printf("%d %d",n1,n);
+         	return 0;
+}
