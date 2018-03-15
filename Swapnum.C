de5da36fd81ee0131574bd795613a5f7ee788954
nandhinic2795 <30898117+nandhinic2795@users.noreply.github.com>
2018-03-15 18:55:22 +0530

    Update and rename Swap.c to Swapnum.C

--- Swapnum.C
+++ Swapnum.C
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 int main(void) {
-         int n,n1;
+         int n,n1,t;
          scanf("%d",&n1);
         scanf("%d",&n);
-        n1=(n1+n)-(n=n1);
+         t=n1;
+         n1=n;
+         n=t;
         printf("%d %d",n1,n);
          	return 0;
 }
