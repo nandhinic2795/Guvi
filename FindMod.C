3daa58aca725b2234eef1860792417113ae33e00
nandhinic2795 <30898117+nandhinic2795@users.noreply.github.com>
2018-03-15 13:54:17 +0530

    Create FindMod.C

---
+++ FindMod.C
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+	int n,d,r,v=0;
+  scanf("%d",&n);
+ scanf("%d",&d);
+ scanf("%d",&r);
+ v=(n*d)%r;
+ printf("%d",v);
+	// your code goes here
+	return 0;
+}
